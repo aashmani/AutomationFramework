7f4440e277678464495e131cfc0454706886e66f
jayarajp <jayarajpmca@gmail.com>
2017-12-21 19:47:41 +0530

    Modified Create Account

--- Microsoft.Dynamics365.UIAutomation.Sample/CreateOpportunity.cs
+++ Microsoft.Dynamics365.UIAutomation.Sample/CreateOpportunity.cs
@@ -4,7 +4,6 @@ using Microsoft.Dynamics365.UIAutomation.Browser;
 using System;
 using System.Security;
 using Microsoft.Dynamics365.UIAutomation.Utility;
-using OpenQA.Selenium;
 
 namespace Microsoft.Dynamics365.UIAutomation.Sample
 {
@@ -15,85 +14,34 @@ namespace Microsoft.Dynamics365.UIAutomation.Sample
         private SecureString _username = string.Empty.ToSecureString();
         private readonly SecureString _password = string.Empty.ToSecureString();
         private readonly Uri _xrmUri;
-        private readonly BrowserType _browser;
+        public static XrmBrowser xrmBrowser = new XrmBrowser(TestSettings.Options);
 
         [TestMethod]
         public void TestCreateNewOpportunity()
         {
-            using (var xrmBrowser = new XrmBrowser(TestSettings.Options))
+            Opportunity.xrmBrowser = xrmBrowser;
+            try
             {
-                Random rnd = new Random();
-            
-                Logs.LogHTML(string.Empty, Logs.HTMLSection.Header, Logs.TestStatus.NA, this.GetType().Name, Helper.SecureStringToString(_username), _browser.ToString());
 
-                xrmBrowser.LoginPage.Login(_xrmUri, _username, _password);
-                xrmBrowser.GuidedHelp.CloseGuidedHelp();
-                Logs.LogHTML("Logged in Successfully", Logs.HTMLSection.Details, Logs.TestStatus.Pass);
+                BaseModel.Login(xrmBrowser, _xrmUri, _username, _password, this.GetType().Name);
+                Opportunity.Navigate();
 
-                xrmBrowser.ThinkTime(500);
-                xrmBrowser.Navigation.OpenSubArea("Sales", "Opportunities");
-                Logs.LogHTML("Navigated to Opportunities  Successfully", Logs.HTMLSection.Details, Logs.TestStatus.Pass);
-                
-                xrmBrowser.ThinkTime(200);
-                xrmBrowser.Grid.SwitchView("Open Opportunities");
-
-                xrmBrowser.ThinkTime(1000);
-                xrmBrowser.CommandBar.ClickCommand("New");
-
-                xrmBrowser.ThinkTime(5000);
-
-                string oppName= "Test API Opportunity" + rnd.Next(100000, 999999).ToString();
-                xrmBrowser.Entity.SetValue("name", oppName);
-                xrmBrowser.Entity.SetValue("description", "Testing the create api for Opportunity");
-
-                xrmBrowser.Entity.SetValue("new_deligatedto", "Test new deligatedto ");
-                
-
-                xrmBrowser.CommandBar.ClickCommand("Save & Close");
-                xrmBrowser.ThinkTime(5000);
-
-                if (xrmBrowser.Driver.IsVisible(By.Id("InlineDialog_Background")))
+                string createdName = Opportunity.Create();
+                if (Opportunity.Search(createdName))
                 {
-                    xrmBrowser.Dialogs.DuplicateDetection(true);
-                    xrmBrowser.ThinkTime(2000);
-                    Logs.LogHTML("Duplicate Opportunities Found", Logs.HTMLSection.Details, Logs.TestStatus.Pass);
-
-                }
-
-                xrmBrowser.Grid.Search(oppName);
-                xrmBrowser.ThinkTime(1000);
-
-                var results = xrmBrowser.Grid.GetGridItems();
-
-                if (results.Value == null || results.Value.Count == 0)
-                {
-                    Logs.LogHTML("Opportunity  not found or was not created.", Logs.HTMLSection.Details, Logs.TestStatus.Fail);
-                }
-                else
-                {
-                    Logs.LogHTML("Created Opportunity  Successfully", Logs.HTMLSection.Details, Logs.TestStatus.Pass);
-                }
-
-
-                try
-                {
-                    xrmBrowser.ThinkTime(1000);
-                    xrmBrowser.Grid.SelectRecord(0);
-                    Logs.LogHTML("Selected Opportunity to Delete", Logs.HTMLSection.Details, Logs.TestStatus.Pass);
-
-
-                    xrmBrowser.CommandBar.ClickCommand("Delete");
-                    xrmBrowser.ThinkTime(2000);
-                    xrmBrowser.Dialogs.Delete();
-                    Logs.LogHTML("Deleted Opportunity Successfully", Logs.HTMLSection.Details, Logs.TestStatus.Pass);
-
-                }
-                catch (Exception ex)
-                {
-                    xrmBrowser.ThinkTime(1000);
-                    Logs.LogHTML("Delete Opportunity ( " + oppName + " ) Failed : " + ex.Message, Logs.HTMLSection.Details, Logs.TestStatus.Fail);
+                    Logs.LogHTML("Created Opportunity Successfully", Logs.HTMLSection.Details, Logs.TestStatus.Pass);
+                    Opportunity.Delete();
                 }
             }
+            catch (Exception ex)
+            {
+                BaseModel.LogError(ex.Message, this.GetType().Name);
+            }
+            finally
+            {
+                Opportunity.Close();
+            }
+
         }
     }
 }
